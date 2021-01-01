f35e0ff15c41fe92caa0b8169b172e38d36da4cc
Bimal Chandra Debnath <bimal.nsu.cse@gmail.com>
2021-01-02 00:30:31 +0600

    laravel multi-diemision array respond test ok

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,11 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use json;
 class StudentController extends Controller
 {
     public function index()
     {
-        return view('student.index');
+        // return response()
+        //     ->view('student.index', json([
+        //         'name' => 'Abigail',
+        //         'state' => 'CA',
+        //     ]), 200);
+        // return view('student.index')
+        
+        // ->json([
+        //     'name' => 'Abigail',
+        //     'state' => 'CA',
+        // ]);
+        // return Response::view('hello')->header('Content-Type', $type);
+        // return view('student.index',compact(json([
+        //     'name' => 'Abigail',
+        //     'state' => 'CA',
+        // ])));
+        // $pizza = [[1,2],[2,3],[3,4]];
+        // $myObj->name = "John";
+        // $myObj->age = 30;
+        // $myObj->city = "New York";
+        // return view('student.index',['pizza' => $pizza]);
+
+        $exams = array(
+                        array(
+                        'id'  =>1,
+                        'name'  =>'exam 1',
+                        'description'=>'Syllabus: chapter 1,2' 
+                        ),
+                        array(
+                            'id'  =>2,
+                            'name' => 'exam 2',
+                            'description'=>'Syllabus: chapter 1,2' 
+                            )
+                    );
+        return view('student.index',['exams' => $exams]);            
     }
 }
