49966e37fdbb3cb0b4b891e12f8f91f0e7733783
Bimal Chandra Debnath <bimal.nsu.cse@gmail.com>
2021-01-12 00:24:40 +0600

    update teacher's course content page

--- resources/views/teacher/index.blade.php
+++ resources/views/teacher/index.blade.php
@@ -1,23 +1,108 @@
-@extends('layouts.app')
+@extends('master')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+ @section('title_area') 
+	 Dasboard
+ @endsection() 
+@section('style_area')
+    <style type="text/css">
+        .qpaper{
+            width:900px;
+            background-color:rgb(0,0,0,0.6);
+            margin:auto;
+            color:#FFFFFF;
+            padding:10px 0px 10px 10px; 
+            text-align:center; 
+            border-radius:15px 15px 0px 0px;
+            }
+            
+        .qbody{
+            width:900px;				
+            background-color:rgb(240, 240, 240);
+            margin:auto;				
+            padding:10px 0px 10px 10px; 
+            border-radius:15px 15px 0px 0px;
+            }
+            
+        .submit{
+            position:relative;
+            left:200px;
+            top:-37px;
+            line-height:40px;
+            width:180px;
+            border-radius:6px;
+            padding:0 22px;
+            font-size:16px;
+            color:#455;
+        }
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+
+    </style>
+@endsection()        
+
+
+@section('content_area')
+ 
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box">
+            <div class="box-header">
+                <h3 class="box-title">exam list</h3>
+                <div class="box-tools">
+                    <div class="input-group input-group-sm" style="width: 150px;">
+                        <input type="text" name="table_search" class="form-control pull-right" placeholder="Search">
+
+                        <div class="input-group-btn">
+                            <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
                         </div>
-                    @endif
-
-                    {{ __('Hello teacher, You are logged in!') }}
+                    </div>
                 </div>
             </div>
+            <!-- /.box-header -->
+            <div class="box-body table-responsive no-padding">
+                <table class="table table-hover">
+                    <tr>
+                        <th>ID</th>
+                        <th>exam name</th>
+                        <th>description</th>
+                        <th>action</th>
+                    </tr>
+                    <tr>
+                        <td>183</td>
+                        <td>exam_1</td>
+                        <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                        <td>
+                            <button class="btn btn-secondary">Edit</button>
+                            <button class="btn btn-danger">delete</button>                            
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>133</td>
+                        <td>exam_2</td>
+                        <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                        <td>
+                            <button class="btn btn-secondary">Edit</button>
+                            <button class="btn btn-danger">delete</button>                             
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>150</td>
+                        <td>exam_4</td>
+                        <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                        <td>
+                            <button class="btn btn-secondary">Edit</button>
+                            <button class="btn btn-danger">delete</button>                            
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <!-- /.box-body -->
         </div>
+        <!-- /.box -->
     </div>
 </div>
-@endsection
+
+@endsection()
+
+@section('script_area')
+    
+@endsection()
