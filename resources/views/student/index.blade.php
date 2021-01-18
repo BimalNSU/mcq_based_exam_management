ae8f0fc5b69989ffb8dfe7183393fb6129cab657
Bimal Chandra Debnath <bimal.nsu.cse@gmail.com>
2021-01-19 00:32:29 +0600

    extra html part removed

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -56,37 +56,9 @@
         </div>
     </div>  
 @endforeach
-<div class="row">
-    <div class="col-md-12">
-        <div class="box box-light">
-            <div class="box-header">
-                <a href="">
-                    <h3 class="box-title">Exam 1</h3>
-                </a>
-            </div>
-            <div class="box-body">
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> 
-            </div>
-        </div>    
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-md-12">
-        <div class="box box-light">
-            <div class="box-header">
-                <a href="">
-                    <h3 class="box-title">Exam 1</h3>
-                </a>
-            </div>
-            <div class="box-body">
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> 
-            </div>
-        </div>    
-    </div>
-</div>
 
 @endsection()
+
 
 @section('script_area')
     
