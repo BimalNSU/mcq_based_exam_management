9f7139e701ed27614583732654f6fd260e1822f2
Bimal Chandra Debnath <bimal.nsu.cse@gmail.com>
2021-01-30 01:39:51 +0600

    exam info page update for student portal

--- resources/views/student/exam_info.blade.php
+++ resources/views/student/exam_info.blade.php
@@ -42,6 +42,45 @@
         <h4> Time limits: {{$exam_info['time_limit']}} minutes</h4> 
         <div id='attempt'></div>
     </div>
+
+	<br>
+	@if($exam_result)
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="box">
+				<div class="box-header">
+					<h3 class="box-title">Attempt list</h3>
+				</div>
+				<!-- /.box-header -->
+				<div class="box-body table-responsive no-padding">
+					<table class="table table-hover" id="">
+						<tr>
+							<th>Attempt no</th>
+							<th>Attempt start</th>
+							<th>Total marks</th>
+							<th></th>
+						</tr>
+						@foreach($exam_result as $value)
+							<tr>
+								<td>{{$value['attempt_no']}}</td>
+								<td>{{$value['total_marks']}}</td>
+								<td>
+									<a href= "{{url('/teacher/exam/review',$value['exam_track_id'])}}"> 
+										<button type="button" class="btn btn-success">
+											Review
+										</button>
+									</a>                          
+								</td>
+							</tr>
+						@endforeach                   
+					</table>
+				</div>
+				<!-- /.box-body -->
+			</div>
+			<!-- /.box -->
+		</div>
+	</div>
+	@endif
 @endsection()
 
 
@@ -49,7 +88,9 @@
  <script>
 	let attempt_btn = {!! json_encode($attempt_btn) !!};
 	let exam_id = {!! json_encode($exam_id) !!};
-	// console.log(attempt_btn);
+	let data3 = {!! json_encode($exam_result) !!};
+	
+	console.log(data3);
 	$('#attempt').html(attempt_btn);
 	// document.getElementById("link").href = '/mcq_based_exam/public/student/exam/request/'+exam_id;
 	document.getElementById("link").href = "{{ route('requestExam', [':exam_id']) }}".replace(':exam_id', exam_id);
