ed16b53e46110c82d60065a191274a69f312c8db
Bimal Chandra Debnath <bimal.nsu.cse@gmail.com>
2021-02-01 21:58:43 +0600

    added group middlewire and prefix together

--- routes/web.php
+++ routes/web.php
@@ -20,25 +20,29 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/admin','AdminController@index')->name('admin')->middleware('admin');
-Route::get('/teacher','TeacherController@index')->name('teacher')->middleware('teacher');
 
-Route::GET('teacher/exam/create','TeacherController@create_exam_page')->middleware('teacher');
-Route::POST('teacher/exam/insert','TeacherController@create_exam_to_course')->name('createExam')->middleware('teacher');
-Route::GET('/teacher/exam/edit_view/{exam_id}','TeacherController@get_exam_data')->middleware('teacher');
-Route::POST('teacher/exam/edit/{exam_id}','TeacherController@update_exam_of_course')->middleware('teacher');
-Route::GET('/teacher/exam/view/{exam_id}','TeacherController@exam_info')->middleware('teacher');
-Route::delete('teacher/exam/delete/{exam_id}','TeacherController@delete_exam_of_course')->name('deleteExam')->middleware('teacher');
+Route::group(['prefix' => 'teacher',  'middleware' => 'teacher'],function () {
+    Route::get('/','TeacherController@index')->name('teacher');
+    Route::GET('exam/create','TeacherController@create_exam_page');
+    Route::POST('exam/insert','TeacherController@create_exam_to_course')->name('createExam');
+    Route::GET('exam/edit_view/{exam_id}','TeacherController@get_exam_data');
+    Route::POST('exam/edit/{exam_id}','TeacherController@update_exam_of_course');
+    Route::GET('exam/view/{exam_id}','TeacherController@exam_info');
+    Route::delete('exam/delete/{exam_id}','TeacherController@delete_exam_of_course')->name('deleteExam');
 
-Route::GET('teacher/exam/{exam_id}/question/create','TeacherController@create_question_page')->name('createQuestionPage')->middleware('teacher');
-Route::post('teacher/exam/{exam_id}/question/insert/','TeacherController@create_question_to_exam')->name('createQuestion')->middleware('teacher');
-Route::GET('/teacher/exam/{exam_id}/question/edit_view/{q_track_id}','TeacherController@get_exam_questions_details')->name('questionEditView')->middleware('teacher');
-Route::post('teacher/exam/{exam_id}/question/edit/{q_track_id}','TeacherController@update_question_of_exam')->name('updateQuestion')->middleware('teacher');
-Route::delete('teacher/exam/question/delete/{q_track_id}','TeacherController@delete_exam_question')->name('deleteExamQuestion')->middleware('teacher');
+    Route::GET('exam/{exam_id}/question/create','TeacherController@create_question_page')->name('createQuestionPage');
+    Route::post('exam/{exam_id}/question/insert/','TeacherController@create_question_to_exam')->name('createQuestion');
+    Route::GET('exam/{exam_id}/question/edit_view/{q_track_id}','TeacherController@get_exam_questions_details')->name('questionEditView');
+    Route::post('exam/{exam_id}/question/edit/{q_track_id}','TeacherController@update_question_of_exam')->name('updateQuestion');
+    Route::delete('exam/question/delete/{q_track_id}','TeacherController@delete_exam_question')->name('deleteExamQuestion');
 
-Route::get('/student','StudentController@index')->name('student')->middleware('student');
-Route::get('/student/exam/view/{exam_id}','StudentController@exam_info')->middleware('student');
-Route::get('/student/exam/request/{exam_id}','StudentController@join_request_exam')->name('requestExam')->middleware('student');
-Route::get('/student/exam/join/{exam_track_id}','StudentController@join_exam_page')->name('join_exam')->middleware('student');
-Route::post('/student/exam/do/{exam_track_id}','StudentController@do_exam')->name('do_exam')->middleware('student');
-Route::get('/student/exam/review/{exam_track_id}','StudentController@exam_review')->name('exam_review')->middleware('student');
-  
+});
+
+Route::group(['prefix' => 'student',  'middleware' => 'student'],function () {
+    Route::get('/','StudentController@index')->name('student');
+    Route::get('exam/view/{exam_id}','StudentController@exam_info');
+    Route::get('exam/request/{exam_id}','StudentController@join_request_exam')->name('requestExam');
+    Route::get('exam/join/{exam_track_id}','StudentController@join_exam_page')->name('join_exam');
+    Route::post('exam/do/{exam_track_id}','StudentController@do_exam')->name('do_exam');
+    Route::get('exam/review/{exam_track_id}','StudentController@exam_review')->name('exam_review');
+});
